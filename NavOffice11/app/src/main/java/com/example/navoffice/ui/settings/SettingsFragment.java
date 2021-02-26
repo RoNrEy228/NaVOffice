53604fdf463cfe378cb6786e18bdfdf36beac484
AskarKasimov <59764983+AskarKasimov@users.noreply.github.com>
2021-02-26 12:22:19 +0400

    Update SettingsFragment.java

--- NavOffice11/app/src/main/java/com/example/navoffice/ui/settings/SettingsFragment.java
+++ NavOffice11/app/src/main/java/com/example/navoffice/ui/settings/SettingsFragment.java
@@ -1,10 +1,22 @@
 package com.example.navoffice.ui.settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,25 +24,88 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.navoffice.MainActivity;
 import com.example.navoffice.R;
 
 public class SettingsFragment extends Fragment {
+    public void download(Context context) {
+        String url = "https://upload.wikimedia.org/wikipedia/commons/5/5d/James_Douglas_Davison_crop.jpg";
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE |
+                DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle("timetable");
+        request.setDescription("NaVOffice DB");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "timetable.jpg");
+        DownloadManager manager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+    }
+    public boolean isNetworkOnline(Context context) {
+        boolean status = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.getState() == NetworkInfo.State.CONNECTED) {
+                status = true;
+            } else {
+                netInfo = cm.getActiveNetworkInfo();
+                if (netInfo != null && netInfo.getState() == NetworkInfo.State.CONNECTED)
+                    status = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return status;
 
+    }
 
     private SettingsViewModel settingsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+
+
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+                Button button = (Button) getActivity().findViewById(R.id.button);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (!isNetworkOnline(getActivity())) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            builder.setTitle("Ошибка 522")
+                                    .setMessage("Проверьте подключение к интернету и повторите попытку!")
+                                    .setCancelable(false)
+                                    .setNegativeButton("ОК",
+                                            new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int id) {
+                                                    dialog.cancel();
+                                                }
+                                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                        }
+                        else {
+                                download(getActivity());
+
+                            Toast.makeText(getActivity(), "База данных обновлена", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
             }
         });
+
         return root;
+
     }
+
 }
