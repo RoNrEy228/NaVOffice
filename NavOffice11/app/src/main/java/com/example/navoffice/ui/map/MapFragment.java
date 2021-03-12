9caa49bf3765af28251402122b5bc5fa26b5b789
AskarKasimov <59764983+AskarKasimov@users.noreply.github.com>
2021-03-12 21:31:22 +0400

    обнова

--- NavOffice11/app/src/main/java/com/example/navoffice/ui/map/MapFragment.java
+++ NavOffice11/app/src/main/java/com/example/navoffice/ui/map/MapFragment.java
@@ -1,7 +1,10 @@
 package com.example.navoffice.ui.map;
-
+import com.example.navoffice.*;
 import android.app.DownloadManager;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,12 +23,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.navoffice.DBHelper;
 import com.example.navoffice.MainActivity;
 import com.example.navoffice.R;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MapFragment extends Fragment {
 
@@ -43,10 +49,65 @@ public class MapFragment extends Fragment {
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Date data = new Date();
-                        int hours = Calendar.HOUR_OF_DAY;
+                        File dbshka = new File(Environment.DIRECTORY_DOWNLOADS + "/db.db");
+                        if (!dbshka.exists()) {
+                            String url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/cG5HA1cS9UVR3w";
+                            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE |
+                                    DownloadManager.Request.NETWORK_WIFI);
+                            request.setTitle("db");
+                            request.setDescription("NaVOffice DB");
+                            request.allowScanningByMediaScanner();
+                            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "db.db");
+                            DownloadManager manager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                            manager.enqueue(request);
+                            try {
+                                TimeUnit.SECONDS.sleep(5);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        DBHelper dbHelper = new DBHelper(getActivity());
 
-                        Log.i("ggeg", String.valueOf(hours));
+                        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+                        ContentValues contentValues = new ContentValues();
+
+
+
+
+                        //contentValues.put(DBHelper.KEY_NAME, "hello");
+                        //contentValues.put(DBHelper.KEY_MAIL, "maillll");
+
+                        //database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                        //добавление
+
+                        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+
+                        if (cursor.moveToFirst()) {
+                            int nameRoom = cursor.getColumnIndex(DBHelper.KEY_ROOM);
+                            int nameGroup = cursor.getColumnIndex(DBHelper.KEY_GROUP);
+                            int nameDay = cursor.getColumnIndex(DBHelper.KEY_DAY);
+                            do {
+                                Log.i("DB:", "GROUP = " + cursor.getString(nameGroup) + ", DAY = " + cursor.getString(nameDay) + ", ROOM = " + cursor.getString(nameRoom));
+                                // Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                                //       ", name = " + cursor.getString(nameIndex) +
+                                //      ", email = " + cursor.getString(emailIndex));
+                            }
+                            while (cursor.moveToNext()) ;
+                        }
+                        //Button btn = (Button) findViewById(R.id.button);
+                        //отображение
+
+/*
+                database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                    //удаление
+  */
+                        cursor.close();
+                        dbHelper.close();
+
+                        Date data = new Date();
                         //  НИЖЕ КОММЕНТ ЧЕК
                         //  ДИМА, ЭТА ШТУКА СКРЫВАЕТ IT КАБИНЕТ!!!
                         //  ВЫШЕ КОММЕНТ ЧЕК
