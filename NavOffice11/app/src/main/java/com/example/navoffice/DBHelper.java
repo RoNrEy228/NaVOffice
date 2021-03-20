98a23a2ebcc1748725a5d6b0d6bfe58907856d89
AskarKasimov <59764983+AskarKasimov@users.noreply.github.com>
2021-03-20 16:11:43 +0400

    +еще одна обнова

--- NavOffice11/app/src/main/java/com/example/navoffice/DBHelper.java
+++ NavOffice11/app/src/main/java/com/example/navoffice/DBHelper.java
@@ -2,32 +2,35 @@ package com.example.navoffice;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
 
 public class DBHelper  extends SQLiteOpenHelper{
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static int DATABASE_VERSION = 3;
+    public static final String TABLE_CONTACTS = "main";
 
-    public static final String KEY_ID = "_id";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_ROOM = "Room";
+    public static final String KEY_DAY = "Day_of_ week";
 
     public DBHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, Environment.getExternalStorageDirectory() + "/Download/test.db/", null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-               + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+        //db.execSQL("create table " + TABLE_CONTACTS + " (" + KEY_GROUP
+         //      + " integer, " + KEY_DAY + " text, " + KEY_ROOM + " text" + ");");
+        db.rawQuery("SELECT * FROM main", null);
 
-        //db.execSQL("SELECT * FROM " + TABLE_CONTACTS);
+        Log.i("eww", "owjgw");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-       // db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        // db.execSQL("drop table if exists " + TABLE_CONTACTS);
 
         onCreate(db);
 
