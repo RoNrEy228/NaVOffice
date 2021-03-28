24d954970a4e8143b7d9371350b58f8c15a8fd37
AskarKasimov <59764983+AskarKasimov@users.noreply.github.com>
2021-03-28 18:03:49 +0400

    БД теперь работает, также добавил диалоговое окно при отсутствии БД

--- NavOffice11/app/src/main/java/com/example/navoffice/ui/map/MapFragment.java
+++ NavOffice11/app/src/main/java/com/example/navoffice/ui/map/MapFragment.java
@@ -1,15 +1,14 @@
 package com.example.navoffice.ui.map;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,6 +16,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.navoffice.DBHelper;
 import com.example.navoffice.R;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MapFragment extends Fragment {
 
@@ -30,74 +32,91 @@ public class MapFragment extends Fragment {
         mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                Button button = (Button) getActivity().findViewById(R.id.button2);
-                button.setOnClickListener(new View.OnClickListener() {
+                getActivity().findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        /*File dbshka = new File(Environment.DIRECTORY_DOWNLOADS + "test.db");
-                        if (!dbshka.exists()) {
-                            String url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/cG5HA1cS9UVR3w";
-                            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE |
-                                    DownloadManager.Request.NETWORK_WIFI);
-                            request.setTitle("test");
-                            request.setDescription("NaVOffice DB");
-                            request.allowScanningByMediaScanner();
-                            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "test.db");
-                            DownloadManager manager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-                            manager.enqueue(request);
-                        }
+                        try {
+                            DBHelper dbHelper = new DBHelper(getActivity());
 
-                         */
-                        DBHelper dbHelper = new DBHelper(getActivity());
-
-                        SQLiteDatabase database = dbHelper.getWritableDatabase();
+                            SQLiteDatabase database = new DBHelper(getActivity()).getWritableDatabase();
 
 
-                        //ContentValues contentValues = new ContentValues();
+                            //ContentValues contentValues = new ContentValues();
 
 
+                            //contentValues.put(DBHelper.KEY_NAME, "hello");
+                            //contentValues.put(DBHelper.KEY_MAIL, "maillll");
+
+                            //database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                            //добавление
 
 
-                        //contentValues.put(DBHelper.KEY_NAME, "hello");
-                        //contentValues.put(DBHelper.KEY_MAIL, "maillll");
-
-                        //database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                        //добавление
-
-                        Cursor cursor = database.query("main", null, null, null, null, null, null);
-                        if (cursor.moveToFirst()) {
-                            int nameRoom = cursor.getColumnIndex("Room");
-                            int nameDay = cursor.getColumnIndex(DBHelper.KEY_DAY);
-                            do {
-                                Toast.makeText(getActivity(), cursor.getString(nameRoom) + cursor.getString(nameDay), Toast.LENGTH_LONG).show();
-                                //Log.i("DB:", "ROOM = " + cursor.getString(nameRoom));
-                                // Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                //       ", name = " + cursor.getString(nameIndex) +
-                                //      ", email = " + cursor.getString(emailIndex));
+                            Cursor cursor = database.rawQuery("SELECT * FROM main", null);
+                            if (cursor.moveToFirst()) {
+                                int nameRoom = cursor.getColumnIndex("Room");
+                                int nameDay = cursor.getColumnIndex(DBHelper.KEY_DAY);
+                                do {
+                                    Toast.makeText(getActivity(), cursor.getString(nameRoom) + cursor.getString(nameDay), Toast.LENGTH_LONG).show();
+                                    //Log.i("DB:", "ROOM = " + cursor.getString(nameRoom));
+                                    // Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                                    //       ", name = " + cursor.getString(nameIndex) +
+                                    //      ", email = " + cursor.getString(emailIndex));
+                                }
+                                while (cursor.moveToNext());
                             }
-                            while (cursor.moveToNext()) ;
-                        }
-                        else {
-                            Toast.makeText(getActivity(), "roghwoigw", Toast.LENGTH_LONG).show();
-                        }
-                        //Button btn = (Button) findViewById(R.id.button);
-                        //отображение
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                builder.setTitle("База Данных повреждена")
+                                        .setMessage("Перейдите в раздел настроек и обновите Базу")
+                                        .setCancelable(false)
+                                        .setNegativeButton("ОК",
+                                                new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int id) {
+                                                        dialog.cancel();
+                                                    }
+                                                });
+                                AlertDialog alert = builder.create();
+                                alert.show();
+                            }
+                            //Button btn = (Button) findViewById(R.id.button);
+                            //отображение
 
 /*
                 database.delete(DBHelper.TABLE_CONTACTS, null, null);
                     //удаление
   */
-                        cursor.close();
-                        dbHelper.close();
+                            cursor.close();
+                            dbHelper.close();
 
-                        //Date data = new Date();
-                        //  НИЖЕ КОММЕНТ ЧЕК
-                        //  ДИМА, ЭТА ШТУКА СКРЫВАЕТ IT КАБИНЕТ!!!
-                        //  ВЫШЕ КОММЕНТ ЧЕК
-                        //ImageView it = (ImageView) getActivity().findViewById(R.id.it_cab);
-                        //it.setVisibility(View.VISIBLE);
+                            //ImageView it = (ImageView) getActivity().findViewById(R.id.it_cab);
+                            //it.setVisibility(View.VISIBLE);
+                        }
+                        catch (Exception e) {
+                            File file = new File(Environment.getExternalStorageDirectory() + "/Download/test.db/");
+                            file.delete();
+                            if(file.exists()){
+                                try {
+                                    file.getCanonicalFile().delete();
+                                } catch (IOException ioException) {
+                                    ioException.printStackTrace();
+                                }
+                                if(file.exists()){
+                                    getActivity().deleteFile(file.getName());
+                                }
+                            }
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            builder.setTitle("База Данных не найдена")
+                                    .setMessage("Перейдите в раздел настроек и обновите Базу")
+                                    .setCancelable(false)
+                                    .setNegativeButton("ОК",
+                                            new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int id) {
+                                                    dialog.cancel();
+                                                }
+                                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                        }
                     }
                 });
 
